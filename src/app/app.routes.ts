6a507e246314b97e0f8445b7caa44091e2ac934b
Alan Sousa <alanssrv@outlook.com>
2025-03-25 11:22:48 -0300

    feat: aplicando animacoes e rota de detalhamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
     },
+    {
+        path: 'detail/:id',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    }
 ];
--- src/app/components/list-items/list-items.component.ts
+++ src/app/components/list-items/list-items.component.ts
@@ -1,11 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { NarutoApiService } from '../../services/naruto-api.service';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-list-items',
   imports: [],
   templateUrl: './list-items.component.html',
   styleUrl: './list-items.component.scss',
+  animations: [
+    trigger('fade-in', [
+      transition(':enter', [
+        style({ opacity: 0.1 }),
+        animate('5s')
+      ])
+    ]),
+    trigger('rotate-img', [
+      transition('* => *', [
+        animate('5s infinite', keyframes([
+          style({ transform: 'rotate(0deg)', offset: 0 }),
+          style({ transform: 'rotate(360deg)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class ListItemsComponent implements OnInit {
   #narutoCharactersApi = inject(NarutoApiService);
